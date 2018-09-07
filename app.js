8b9299ce116e97d25b68ed4e95344f6ee86394c3
Paul Bartlett <paul.bartlett@everyonecounts.com>
2018-09-07 10:13:20 -0700

    Add babel - doesnt yet work due to babel v7 migration issue

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-import { Map } from "immutable";
+// A little bit of ES6 import (badly supported except for webpack)
+// import { Map } from "immutable";
 
-console.log('using webpack and immutable');
+console.log('using webpack and babel no immutable');
+
+// A little bit of ES6 to prove babel right
+const a = [1,2,3,4,5];
+a.map(v => console.log(v));
 
 
