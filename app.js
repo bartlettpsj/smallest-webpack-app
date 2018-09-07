4d1ebe69b76e83d2efcf3619fd09c6d5f9edb389
Paul Bartlett <paul.bartlett@everyonecounts.com>
2018-09-07 11:34:17 -0700

    Make babel work

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-// A little bit of ES6 import (badly supported except for webpack)
-// import { Map } from "immutable";
+console.log('Using webpack and babel and immutable');
 
-console.log('using webpack and babel no immutable');
+// A little bit of ES6 import (badly supported except for webpack)
+import { Map } from "immutable";
+
+// Immutable experiment
+const first = Map({key: "value"});
+console.log(first);
+const second = first.set("two", "second");
+console.log(second);
 
 // A little bit of ES6 to prove babel right
 const a = [1,2,3,4,5];
 a.map(v => console.log(v));
-
-
